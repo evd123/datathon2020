0dea01aca648c8bdb3aaeb5717ce25266b240ed0
Steven Feng <48143030+stvfng@users.noreply.github.com>
2020-01-25 06:08:35 -0600

    Add files via upload
    jk this is it

--- chevy_gang.R
+++ chevy_gang.R
@@ -1,4 +1,7 @@
+library(nlme)
+
 chevron <- read.csv("C:/Users/5stev/Documents/chevronChallenge/filesForStartOfDatathon/training.csv")
+#Making depth variable
 chevron_training <- chevron[1:6154,]
 chevron_testing <- chevron[6515:6838,]
 
@@ -10,4 +13,29 @@ first_prediction <- predict(first_regression, newdata = chevron_testing)
 all_regression <- lm(rate_of_penetration ~ . -segment_id, data = chevron_training)
 
 #First look at MC
-first_alias <- as.data.frame(alias(all_regression))
+first_alias <- alias(all_regression)
+
+#New regression with depth 
+max_depth_regression <- lm(rate_of_penetration ~ . -segment_id -min_depth, data = chevron_training)
+min_depth_regression <- lm(rate_of_penetration ~ . -segment_id -max_depth, data = chevron_training)
+
+#Checking for non-linearity? 
+first_predictions_max <- predict(max_depth_regression, newdata = chevron_training)
+first_predictions_min <- predict(min_depth_regression, newdata = chevron_training)
+first_residuals_max <-chevron_training$rate_of_penetration - first_predictions_max
+first_residuals_min <-chevron_training$rate_of_penetration - first_predictions_min
+
+#Residual Plots
+plot(first_predictions_max, first_residuals_max); abline(h = 0, lwd = 2, col = "blue")
+
+
+#qQ Plots
+set.seed(334455) # set seed for pseudo-random number generator 
+
+my_plot = function(sample, my_title){
+  qqnorm(sample, main = my_title); qqline(sample, lwd = 2, col = "blue")
+}
+
+(my_plot(first_residuals_max, "first_plot"))
+
+
